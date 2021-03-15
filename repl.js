4d210f750cf6836b4c16c6b0dbe3e0630950143e
Daniel Harvey <danieljamesharvey@gmail.com>
2021-03-15 08:15:09 +0000

    Add type signature for repl

--- repl.js
+++ repl.js
@@ -1,9 +1,12 @@
-// this part to be added by runtime
-const main = require("../output/cjseb45276f936f01182e2b6d960065a06ac3984ed5b3091d46f8a29fba88fa109d")
-  .main;
-
-// this is all standard boilerplate
 const repl = require("repl");
+
+/*
+  { init: A,
+    next: A -> string -> [A, string],
+    intro: string,
+    prompt: string
+  }
+*/
 
 const createRepl = (mimsaRepl) => {
   let mutableState = mimsaRepl.init;
